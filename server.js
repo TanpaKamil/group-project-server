224006351556f8d8b993a597f7167d5bf9749d4b
dimashadin <dimashadin18@gmail.com>
2025-01-09 12:06:23 +0700

    fix: origin cors

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
         // origin: "http://localhost:5173",
-        origin: "http://https://gp-onlinezoo.web.app",
+        origin: "https://gp-onlinezoo.web.app",
         methods: ["GET", "POST"]
     }
 });
